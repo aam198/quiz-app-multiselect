ad73e9832fab186d443ee1c9d42a4b0c0017f470
aam198 <aam198@psu.edu>
2023-01-17 14:36:52 -0500

    Content updates

--- src/index.js
+++ src/index.js
@@ -99,9 +99,9 @@ submitBtn.addEventListener('click', () => {
     }
     else {
       quiz.innerHTML = `
-        <h2> You answered correctly at ${score}  / ${quizData.length} questions</h2>
+        <h2> You answered ${score}  / ${quizData.length} questions correctly</h2>
         
-        <button onclick="location.reload()">Reload</button>`
+        <button onclick="location.reload()">Reload Quiz</button>`
     }
   }
 })
